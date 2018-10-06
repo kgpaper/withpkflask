62c587c7b3c83357e00909208bf5d8e2d6cc4736
sanha <kgpaper@naver.com>
2018-10-06 19:03:10 +0900

    2018/10/06 navbar update

--- index.py
+++ index.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/home", methods=['GET','POST'])
-def home():
-    return render_template('onsurvey.html')
-
 @app.route("/", methods=['GET','POST'])
+def home():
+    return render_template('nav.html')
+
+@app.route("/ex", methods=['GET','POST'])
 def example():
-    return render_template('example.html')
+    return render_template('temindex.html')
 
 if __name__ == "__main__":
-    app.run(port = 8000)
+    app.run(port = 8080)
